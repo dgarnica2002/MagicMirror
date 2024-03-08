3370f3281f48cad0956678732f9a1e0eccbc193b
dgarnica <dgarnica@2002fchh.com>
2024-03-08 13:29:25 -0800

    added menu for this week

--- config/config.js
+++ config/config.js
@@ -99,7 +99,7 @@ let config = {
 			module: "MMM-Logo",
 			position: "upper_third",
 			config: {
-				maxWidth: "15%",       // Sizes the images. Retains aspect ratio.
+				maxWidth: "23%",       // Sizes the images. Retains aspect ratio.
 				ownImagePath: "/modules/menu.png",      // ex: 'modules/MMM-EyeCandy/pix/YOUR_PICTURE_NAME.jpg', or internet url to image
 			}
 		},
